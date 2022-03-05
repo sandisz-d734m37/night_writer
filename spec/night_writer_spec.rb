2b3009bc798235076ce082a7bda71ed48ba8878a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-04 19:55:50 -0700

    Test: write test asking if NightWriter can translate text to braille

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -45,3 +45,34 @@ RSpec.describe NightWriter do
     expect expected == 7
   end
 end
+
+RSpec.describe "Translatable" do
+  let(:night_writer) { NightWriter.new(arguments) }
+
+  let(:arguments) { [test_message.path, test_braille.path] }
+  let(:test_braille) { Tempfile.new('txt')}
+  let(:test_message) do
+    Tempfile.new('txt').tap do |msg|
+      msg << "nothing"
+      msg.close
+    end
+  end
+
+  after do
+    test_message.unlink
+    test_braille.unlink
+  end
+
+  it "exists" do
+    expect(night_writer).to be_a NightWriter
+  end
+
+  it "can interact with files" do
+    expect(File.open(test_message.path).readlines).to eq(["nothing"])
+  end
+
+  it "can translate english to braille" do
+    night_writer.readfile
+    expect(File.open(test_braille.path).readlines.length).to eq(3)
+  end
+end
